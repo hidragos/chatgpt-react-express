e2e2f230650350e4696b97a8da3e8e1a786f0776
Dragos Andrei <hi.dragos.andrei@gmail.com>
2023-03-22 19:23:04 +0200

    🏗️ Create basic folder structure for components and config

---
+++ src/components/ChatBubble.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ChatBubble = () => {
+  return (
+    <span> chat bubbles zone </span>
+  );
+};
+
+export default ChatBubble;
---
+++ src/components/ChatInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ChatInput = () => {
+  return (
+    <span> chat input zone </span>
+  );
+};
+
+export default ChatInput;
